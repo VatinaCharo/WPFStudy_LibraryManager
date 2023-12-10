f5f6d3d4b5caf7bf7216bf6d5eb07d3b27d13ed3
Eur3ka <njuscimenphy@gmail.com>
2023-12-10 14:02:19 +0800

    new：实现登录功能和属性绑定

--- WPFStudy_LibraryManager/MainWindow.xaml.cs
+++ WPFStudy_LibraryManager/MainWindow.xaml.cs
@@ -1,23 +1,57 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 
 namespace WPFStudy_LibraryManager
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+        private string _UsrName;
+        public string UsrName
+        {
+            get { return _UsrName; }
+            set
+            {
+                _UsrName = value;
+                FirePropertyChangedEvent("UsrName");
+            }
+        }
+        private string _Password;
+        public string Password
+        {
+            get { return _Password; }
+            set
+            {
+                _Password = value;
+                FirePropertyChangedEvent("Password");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void FirePropertyChangedEvent(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var usrName = usrNameTextBox.Text;
-            var usrPwd = usrPasswordTextBox.Text;
-            if (usrName != "wpf" || usrPwd != "123456") { MessageBox.Show("用户名或密码不正确"); return; }
-            MessageBox.Show("OK");
+            if (UsrName != "wpf" || Password != "123456")
+            {
+                MessageBox.Show("用户名或密码不正确");
+                UsrName = string.Empty;
+                Password = string.Empty;
+                return;
+            }
+            var index = new Index();
+            index.Show();
+            Close();
         }
     }
 }
